cd631ca987f9a22f8c764b50e059a298a928e0dd
isynczxc <hfsync@gmail.com>
2026-02-05 22:49:11 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -39,7 +39,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/🚫大胆な'],
+  protectedRoutes: ['/🚫大胆な','/🔒Personal Vault'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: '',
